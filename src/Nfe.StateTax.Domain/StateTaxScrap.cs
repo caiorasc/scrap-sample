e608f35177c2fad14b690f4945f70790f831ae77
Caio Costa <caio@nfe.io>
2018-05-10 23:45:23 +0000

    add Scrap and ReadScrap methods

--- src/Nfe.StateTax.Domain/StateTaxScrap.cs
+++ src/Nfe.StateTax.Domain/StateTaxScrap.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,9 @@ namespace Nfe.StateTax.Domain
     public class StateTaxScrap : IScrap
     {
         private HttpClient _httpClient;
-        public StateTaxScrap(HttpClient httpClient) 
+        public StateTaxScrap(HttpClient httpClient = null) 
         {
-            _httpClient = httpClient;
+            _httpClient = httpClient ?? new HttpClient();
         }
         public async Task<string> PreScrap()
         {
@@ -25,9 +26,40 @@ namespace Nfe.StateTax.Domain
             // pegar a resposta e ler
         }
 
-        public async Task<string> Scrap(long cnpj)
+        public async Task<InfoCnpj> Scrap(long cnpj)
         {
-            throw new NotImplementedException();
+            var test = $"tipoDocumento=cnpj&numrDocumento=0{cnpj}&btnSubmit=";
+
+
+            var content = new StringContent(test, Encoding.UTF8, "application/x-www-form-urlencoded");
+            var infoCnpj = new InfoCnpj();
+            var post = await _httpClient.PostAsync("http://www.sefaz.go.gov.br/ccn/001lstResultado.asp", content);
+
+            if (post.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+
+            var htmlResponse = await post.Content.ReadAsStringAsync();
+
+            return infoCnpj;
+        }
+
+        public async Task<InfoCnpj> ReadScrap(string html)
+        {
+            var tableStartIndex = html.IndexOf("<table");
+            var tableEndIndex = html.IndexOf("</table>");
+            var tableLength = (tableEndIndex + 8) - tableStartIndex;
+            var table = html.Substring(tableStartIndex, tableLength);
+
+            //var tableSplit = table.Split(" ");
+
+
+
+            var infoCnpj = new InfoCnpj();
+
+
+            return infoCnpj;
         }
     }
 }
